2a6455849e0cbdcf4e51a16c61f1592b535b484b
Wagner Torres <wav_torres@hotmail.com>
2025-06-29 12:49:20 -0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,11 @@
+# Define as versões mínimas necessárias para o Terraform e provedores
+terraform {
+  required_version = ">= 1.0.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Versão do provedor AWS
+    }
+  }
+}
